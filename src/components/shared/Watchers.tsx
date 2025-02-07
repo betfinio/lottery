de44c76d124159f922dea1c823a4580523158cf2
Monster <bf.monster@proton.me>
2025-02-07 17:48:39 +0100

    feat: add query invalidation for ticket sold event

--- src/components/shared/Watchers.tsx
+++ src/components/shared/Watchers.tsx
@@ -1,17 +1,18 @@
 import { LOTTERY_ADDRESS } from '@/src/globals';
 import { useSelectedRound } from '@/src/lib/query';
 import { LotteryABI, LotteryRoundABI, ZeroAddress } from '@betfinio/abi';
+import { useQueryClient } from '@tanstack/react-query';
 import { useWatchContractEvent } from 'wagmi';
 
 function Watchers() {
 	const { data: round } = useSelectedRound();
-
+	const queryClient = useQueryClient();
 	useWatchContractEvent({
 		address: round?.address,
 		abi: LotteryRoundABI,
 		eventName: 'TicketSold',
-		onLogs: (logs) => {
-			console.log(logs);
+		onLogs: () => {
+			queryClient.invalidateQueries({ queryKey: ['lottery', 'round'] });
 		},
 	});
 
