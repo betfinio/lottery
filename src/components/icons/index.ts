9c4c8a7fc2520a4b8cae71e0bcc43f5736e52dff
Monster <bf.monster@proton.me>
2025-02-10 12:21:51 +0100

    fix: removed icon from index

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,6 +4,5 @@ import type { ForwardRefExoticComponent, RefAttributes } from 'react';
 export type CustomIconProps = ForwardRefExoticComponent<Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>>;
 
 export * from './GoldStar';
-export * from './Ticket';
 export * from './BronzeStar';
 export * from './SilverStar';
